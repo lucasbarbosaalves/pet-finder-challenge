4a45dc23574081c7795c866de87b49110477c5dc
lucasb <lkab05@hotmail.com>
2025-08-12 17:34:22 -0300

    feat: update environment variable validation and add Prisma client

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,12 +1,13 @@
+import 'dotenv/config';
 import z from 'zod';
 
 const HTTP_PORT = 3333;
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('development'),
-  HTTP_PORT: z.coerce.number().min(1024).max(65535).default(HTTP_PORT),
+  HTTP_PORT: z.coerce.number().default(HTTP_PORT),
   DATABASE_URL: z.string(),
-  JWT_SECRET: z.string(),
+  JWT_SECRET: z.string().default('secret-key'),
 });
 
 const _env = envSchema.safeParse(process.env);
